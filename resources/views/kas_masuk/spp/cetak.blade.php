bac86c00b6609b6b719cd564a443e92fa505d805
adiWieli <dewa17a@gmail.com>
2018-10-26 21:37:46 +0700

    Update resources/views/kas_masuk/spp/cetak.blade.php

--- resources/views/kas_masuk/spp/cetak.blade.php
+++ resources/views/kas_masuk/spp/cetak.blade.php
@@ -30,9 +30,9 @@
 		<div class="container" >
 			<div class="row">
 				<div style="background-color: white;" class="row printArea">
-					<div class="col-sm-12 row" style="margin-top: 15px;border-bottom: 2px solid black;height:160px " >
+					<div class="col-sm-12" style="margin-top: 15px;border-bottom: 2px solid black;height:160px " >
 						<div class="col-sm-2 col-xs-2" style="height: 150px">
-							<img style="width: 100%;" src="{{ asset('storage/uploads/sekolah/original/'.$data->sekolah->s_logo.'') }}">
+							<img style="width: 140px;height: 140px" src="{{ asset('storage/uploads/sekolah/original/'.$data->sekolah->s_logo.'') }}">
 						</div>
 						<div class="col-sm-8 col-xs-8"  align="center" style="height: 150px">
 							<h5 style="color: #3b734c">YAYASAN DARUL ULUM GRESIK</h5>
