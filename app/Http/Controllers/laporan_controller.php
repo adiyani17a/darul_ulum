b6e4af68f2796cc9472b3ec578014fc54759e4fb
adiWieli <dewa17a@gmail.com>
2018-10-27 09:00:42 +0700

    f

--- app/Http/Controllers/laporan_controller.php
+++ app/Http/Controllers/laporan_controller.php
@@ -21,8 +21,42 @@ use Jenssegers\Date\Date;
 
 class laporan_controller extends Controller
 {
-    public function register_jurnal()
+    public function register_jurnal(Request $req)
     {
+
+  //   	if ($req->sdd_sekolah != '') {
+  //         $sdd_sekolah = 'and sdd_sekolah = '."'$req->sdd_sekolah'";
+  //       }else{
+  //         $sdd_sekolah = '';
+  //       }
+
+  //       if ($req->sdd_kelas != '') {
+  //         $sdd_kelas = 'and sdd_kelas = '."'$req->sdd_kelas'";
+  //       }else{
+  //         $sdd_kelas = '';
+  //       }
+
+  //       if ($req->sdd_nama_kelas != '') {
+  //         $sdd_nama_kelas = 'and sdd_nama_kelas = '."'$req->sdd_nama_kelas'";
+  //       }else{
+  //         $sdd_nama_kelas = '';
+  //       }
+
+  //       if ($req->sdd_tahun_ajaran != '') {
+  //         $sdd_tahun_ajaran = 'and sdd_tahun_ajaran = '."'$req->sdd_tahun_ajaran'";
+  //       }else{
+  //         $sdd_tahun_ajaran = '';
+  //       }
+
+
+		// if (Auth::user()->akses('LAPORAN REGISTER JURNAL','global')) {
+		// 	$data = $this->models->jurnal()->whereRaw("sdd_status = 'Setujui' $sdd_kelas $sdd_nama_kelas $sdd_tahun_ajaran")->get();
+		// }else{
+		// 	$sekolah = Auth::User()->sekolah_id;
+		// 	$data = $this->models->siswa_data_diri()->where('sdd_sekolah',$sekolah)->whereRaw("sdd_status = 'Setujui' $sdd_kelas $sdd_nama_kelas $sdd_tahun_ajaran")->get();
+		// }
+
+
     	return view('laporan.register_jurnal.register_jurnal');
     }
 }
--- resources/views/laporan/register_jurnal/register_jurnal.blade.php
+++ resources/views/laporan/register_jurnal/register_jurnal.blade.php
@@ -1,17 +1,12 @@
 <!DOCTYPE html>
 <html>
-{{-- @include('partials._head') --}}
+@include('partials._head')
 <link rel="stylesheet" href="{{asset('assets/node_modules/bootstrap-4/css/bootstrap.min.css')}}">
 <head>
 	<title>YAYASAN DARUL ULUM</title>
   	<link rel="shortcut icon" href="{{asset('assets/images/logo1.png')}}" />
 </head>
 <style type="text/css">
-	@page {
-	   size: 21cm 29.7cm;
-   	 margin: 30mm 45mm 30mm 45mm;
-	}
-
 	@media print
 	{
 		body {margin-left: 0 !important;margin-right: 0 !important;height: 0px;background-color: white !important}
@@ -20,35 +15,74 @@
 		padding-right:50px;
 		padding-left:50px;
 	}
+	.transparency{
+		opacity: 0.7;
+	}
+	.logo{
+		width: 30px;
+		height: 30px;
+	}
+	th{
+		text-align: center;
+	}
+	.nopad{
+		padding: 0px;
+	}
+	ul{
+		 list-style-type: none;
+	    margin: 0;
+	    padding: 0;
+	    overflow: hidden;
+	}
+	ul li{
+		float: right;
+		margin-right: 40px;
+	}
+	label{
+		font-size: 12px;
+	}
 </style>
 
 <body style="background-color: grey;margin-left: 200px;margin-right: 200px;">
-	<header id="navigation" style="padding: 0px 0px;height: 60px;vertical-align: middle;background: rgba(0, 0, 0, 0.4); box-shadow: 0px 2px 5px #444;position: fixed; z-index:2;width: 100%;left: 0">
-		<div class="container" >
-		  <div class="row">
-		    <a href="{{ url('/') }}" class="col-sm-6 nopadding-left" style="padding-top: 20px">
-		      <label style="color: white;cursor: pointer;">YAYASAN DARUL ULUM</label>
-		    </a>
-		  </div>
+	<nav class="navbar transparency fixed-top navbar-dark bg-dark">
+		<div class="container">
+			<div class="col-md-8 nopad">
+				<a class="navbar-brand" href="#">
+					<img class="logo"  src="{{asset('assets/images/logo1.png')}}">
+				</a>
+			</div>
+			<div class="col-md-4 nopad text-light">
+				<ul class="">
+					<li><i class="fa fa-print"></i></li>
+					<li><i class="fa fa-sliders"></i></li>
+				</ul>
+			</div>
 		</div>
-	</header>
-	<div  class="" id="printArea" style="padding-top: 60px;background-color: white">
-    <div class="col-sm-12 height mt-2" >
-		<div class="col-sm-12" style="margin-top: 20px" >
-			<h2 class="black"><b>REGISTER JURNAL</b></h2>
-			<h5 class="black">YAYASAN DARUL ULUM</h5>
-			<p class="black">SDN MEDOKAN AYU</p>
-			<hr class="black" style="border-bottom: 2px solid black">
-		</div>
-		<div class="col-sm-12">
-        	<table class="table">
-        		<tr>
-        			<th>No</th>
-        		</tr>
-        	</table>
-      	</div>
-    </div>
-  </div>
+	</nav>
+	<div class="container" id="printArea" style="margin-top: 80px;background-color: white">
+	    <div class="row col-sm-12 height mt-2" >
+			<div class="col-sm-12" style="margin-top: 20px" >
+				<h2 class="black"><b>REGISTER JURNAL</b></h2>
+				<h5 class="black">YAYASAN DARUL ULUM</h5>
+				<p class="black">SDN MEDOKAN AYU</p>
+				<hr class="black" style="border-bottom: 2px solid black">
+			</div>
+			<div class="col-sm-12" style="font-size: 10px">
+				<label>Transaksi : Bulan September - Oktober</label>
+	        	<table class="table table-bordered bg-secondary text-light">
+	        		<tr>
+	        			<th>Tanggal</th>
+	        			<th>No.Bukti</th>
+	        			<th>No.Perkiraan</th>
+	        			<th>Nama Perkiraan</th>
+	        			<th>Uraian</th>
+	        			<th>Debet</th>
+	        			<th>Kredit</th>
+	        		</tr>
+	        	</table>
+	      	</div>
+	    </div>
+  	</div>
 </body>
 @include('partials._script')
 <script type="text/javascript">
